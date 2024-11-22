e9a604048bc21c70b91da12f7425dbdc0e30ee7e
Charlie <charlieryancurry@gmail.com>
2024-11-22 13:58:33 -0400

    add localization to project

---
+++ Assets/Localization/Scripts/LocalizedTextComponent.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
+
+public class LocalizedTextComponent : MonoBehaviour
+{
+
+    [SerializeField] private string tableReference;
+    [SerializeField] private string localizationKey;
+
+    private LocalizedString localizedString;
+    private Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent = GetComponent<Text>();
+        localizedString = new LocalizedString { TableReference = tableReference, TableEntryReference = localizationKey };
+
+        LocalizationSettings.SelectedLocaleChanged += UpdateText;
+
+        //Sets to french by default
+        //var frenchLocale = LocalizationSettings.AvailableLocales.GetLocale("fr");
+        //LocalizationSettings.SelectedLocale = frenchLocale;
+
+        //UpdateText(frenchLocale);
+    }
+
+    private void OnDestroy()
+    {
+        LocalizationSettings.SelectedLocaleChanged -= UpdateText;
+    }
+
+    // Update is called once per frame
+    void UpdateText(Locale locale)
+    {
+        textComponent.GetComponent<Text>();
+        textComponent.text = localizedString.GetLocalizedString();
+    }
+}
---
+++ Assets/Localization/Scripts/Reader.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Language
+{
+    public string lang;
+    public string title;
+    public string play;
+    public string quit;
+    public string options;
+    public string credits;
+}
+
+public class LanguageData
+{
+    public Language[] languages;
+}
+
+public class Reader : MonoBehaviour
+{
+    public TextAsset jsonFile;
+    public string currentLanguage;
+    private LanguageData languageData;
+
+    public Text titleText;
+    public Text playText;
+    public Text quitText;
+    public Text optionsText;
+    public Text creditsText;
+
+    private void Start()
+    {
+        languageData = JsonUtility.FromJson<LanguageData>(jsonFile.text);
+        SetLanguage(currentLanguage);
+    }
+
+    public void SetLanguage(string newLanguage)
+    {
+        foreach (Language lang in languageData.languages)
+        {
+            if (lang.lang.ToLower() == newLanguage.ToLower())
+            {
+                titleText.text = lang.title;
+                playText.text = lang.play;
+                optionsText.text = lang.options;
+                creditsText.text = lang.credits;
+                quitText.text = lang.quit;
+                return;
+            }
+        }
+    }
+}
